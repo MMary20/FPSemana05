a9028458f3c20b767d194952890bf2d8ae5120fc
Maria Margarida <mmargarida688rgmail.com>
2024-12-06 20:31:30 +0000

    Adding produto.py.

---
+++ produto.py
@@ -0,0 +1,32 @@
+class Produto:
+    pass
+
+    def __init__(self, nome, preco, quantidade):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade = quantidade
+
+    def adicionar_stock(self, quantidade):
+        if quantidade > 0:
+            self.quantidade += quantidade
+            print(1)  
+        else:
+            print(0)  
+
+    def vender(self, quantidade):
+        if quantidade <= self.quantidade:
+            self.quantidade -= quantidade
+            print(1)  
+        else:
+            print(0)  
+
+    def exibir_info(self):
+         print(self.nome + " " + str(self.preco) + " " + str(self.quantidade))
+
+produto1 = Produto("Vaso", 19.99, 100)
+produto1.adicionar_stock(-20)
+produto1.adicionar_stock(20)
+produto1.vender(50)
+produto1.vender(100)
+produto1.exibir_info()
+
